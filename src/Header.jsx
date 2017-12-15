7854d25f5cea80e3e64e1f7c19c661536f2597b1
Julius Hsu <juliushsu07@gmail.com>
2017-12-15 16:36:23 +0000

    set up server folder

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ class Header extends Component {
       <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">letsChat</a>
+            <a class="navbar-brand" href="#">letsCode</a>
           </div>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">Share Code</a></li>
