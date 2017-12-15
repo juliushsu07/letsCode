95ebfe49ec271a8253d4cd00cdde39c5219084d9
vagrant <lovemore_jokonya@yahoo.com>
2017-12-15 12:12:30 +0000

    modified header and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,7 +9,11 @@ class Footer extends Component {
           <div class="navbar-header">
             <a class="navbar-brand" href="#">letsChat</a>
           </div>
-
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Share Code</a></li>
+            <li><a href="#">Chat</a></li>
+            <li><a href="#">Place Call</a></li>
+          </ul>
         </div>
       </nav>
     );
--- src/Header.jsx
+++ src/Header.jsx
@@ -9,12 +9,8 @@ class Header extends Component {
           <div class="navbar-header">
             <a class="navbar-brand" href="#">letsChat</a>
           </div>
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Share Code</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
           </ul>
