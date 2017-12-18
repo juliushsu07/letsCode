21b6e3c5067289a53e89f8a5c41ea7da7da2bc49
vagrant <lovemore_jokonya@yahoo.com>
2017-12-18 16:19:45 +0000

    created props and func to eval code

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,48 @@ import CodeTextArea from './CodeTextArea.jsx';
 import Footer from './Footer.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      code_text: "",
+      evaluated_code:"hmm"
+    }; //end of set initial state Object
+    this.changeTextHandler = this.changeTextHandler.bind(this)
+    this.evaluateCode = this.evaluateCode.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  changeTextHandler (event) {
+    let code_text = event.target.value;
+    console.log(`old code state: ${this.state.code_text}`);
+    this.setState({code_text :code_text });
+    console.log(`new code state: ${this.state.code_text}`);
+    // this.setState({evaluated_code :eval(this.state.code_text) });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    evaluateCode(this.state.code_text);
+    console.log("evaluted code: ", this.state.evaluated_code);
+  }
+
+  evaluateCode (code){
+    // if(event.key === "Enter"){
+      console.log("code_text: ", code);
+      this.setState({evaluated_code :eval(code) });
+
+    // }
+  }
+
   render() {
     return (
       <div>
       <Header />
       <h1>Hello and Lets Code :)</h1>
-      <CodeTextArea/>
+      <CodeTextArea  evaluateCode={this.evaluateCode} changeTextHandler={this.changeTextHandler} handleSubmit={this.handleSubmit}/>
+      <span >result =  {this.state.evaluated_code}</span>
       <Footer/>
       </div>
     );
--- src/CodeTextArea.jsx
+++ src/CodeTextArea.jsx
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 class CodeTextArea extends Component {
 
   render() {
-
+    // console.log(this.props)
     return (
-        <textarea className="form-control" rows="5" id="code_text_area"></textarea>
+      <form  onSubmit={this.props.handleSubmit}>
+        <textarea className="form-control" rows="5" id="code_text_area" onChange= {this.props.changeTextHandler }/>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
