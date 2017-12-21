6ae3bd6b919b3efdfb46c1585fb693178ed3f8d5
vagrant <lovemore_jokonya@yahoo.com>
2017-12-21 17:31:29 +0000

    added form for signup

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,29 +2,29 @@ import React, {Component} from 'react';
 export class Login extends React.Component {
   render() {
     return (
-      <div class="col-md-4 col-md-offset-7">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <span class="glyphicon glyphicon-lock"></span> Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form">
-                    <div class="form-group">
-                        <label for="inputEmail3" class="col-sm-3 control-label">
+      <div className="col-md-4 col-md-offset-7">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <span className="glyphicon glyphicon-lock"></span> Login</div>
+                <div className="panel-body">
+                    <form className="form-horizontal" role="form">
+                    <div className="form-group">
+                        <label htmlFor="inputEmail3" className="col-sm-3 control-label">
                             Email</label>
-                        <div class="col-sm-9">
-                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email" required/>
+                        <div className="col-sm-9">
+                            <input type="email" className="form-control" id="inputEmail3" placeholder="Email" required/>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="inputPassword3" class="col-sm-3 control-label">
+                    <div className="form-group">
+                        <label htmlFor="inputPassword3" className="col-sm-3 control-label">
                             Password</label>
-                        <div class="col-sm-9">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required/>
+                        <div className="col-sm-9">
+                            <input type="password" className="form-control" id="inputPassword3" placeholder="Password" required/>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-9">
-                            <div class="checkbox">
+                    <div className="form-group">
+                        <div className="col-sm-offset-3 col-sm-9">
+                            <div className="checkbox">
                                 <label>
                                     <input type="checkbox"/>
                                     Remember me
@@ -32,18 +32,18 @@ export class Login extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div class="form-group last">
-                        <div class="col-sm-offset-3 col-sm-9">
-                            <button type="submit" class="btn btn-success btn-sm">
+                    <div className="form-group last">
+                        <div className="col-sm-offset-3 col-sm-9">
+                            <button type="submit" className="btn btn-success btn-sm">
                                 Sign in</button>
-                                 <button type="reset" class="btn btn-default btn-sm">
+                                 <button type="reset" className="btn btn-default btn-sm">
                                 Reset</button>
                         </div>
                     </div>
                     </form>
                 </div>
-                <div class="panel-footer">
-                    Not Registred? <a href="http://www.jquery2dotnet.com">Register here</a></div>
+                <div className="panel-footer">
+                    Not Registred? <a href="http://localhost:3000/signup">Register here</a></div>
             </div>
         </div>
     );
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,9 +2,57 @@ import React, {Component} from 'react';
 export class Signup extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Signup</h1>
-      </div>
+      <div className="col-md-4 col-md-offset-7">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <span className="glyphicon glyphicon-lock"></span> Sign Up</div>
+                <div className="panel-body">
+                    <form className="form-horizontal" role="form">
+                    <div className="form-group">
+                        <label htmlFor="inputEmail3" className="col-sm-3 control-label">
+                            Email</label>
+                        <div className="col-sm-9">
+                            <input type="email" className="form-control" id="inputEmail3" placeholder="Email" required/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputPassword3" className="col-sm-3 control-label">
+                            Password</label>
+                        <div className="col-sm-9">
+                            <input type="password" className="form-control" id="inputPassword3" placeholder="Password" required/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputPassword4" className="col-sm-3 control-label">
+                            Re-enter Password</label>
+                        <div className="col-sm-9">
+                            <input type="password" className="form-control" id="inputPassword4" placeholder="Password" required/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-offset-3 col-sm-9">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox"/>
+                                    Remember me
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-group last">
+                        <div className="col-sm-offset-3 col-sm-9">
+                            <button type="submit" className="btn btn-success btn-sm">
+                                Sign Up</button>
+                                 <button type="reset" className="btn btn-default btn-sm">
+                                Reset</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+                <div className="panel-footer">
+                </div>
+            </div>
+        </div>
     );
   }
 }
