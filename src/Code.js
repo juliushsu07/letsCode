98c2db8adf8ab6fdf934adf2999774073640301d
Julius Hsu <juliushsu07@gmail.com>
2018-01-08 21:16:26 +0000

    changed port to PUBLIC_URL

--- src/Code.js
+++ src/Code.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 import Video from './Video.jsx';
 
 require('dotenv').config();
-const PORT = process.env.NODE_ENV || 3001;
+const PORT = process.env.PUBLIC_URL || 3001;
 
 
 const isBrowser = typeof window !== 'undefined';
