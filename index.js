a43e6a525b0a97cf609c0157168c9e7251d2259f
Lovemore <lovemorejokonya@gmail.com>
2018-01-08 00:51:48 -0500

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const expressEnforcesSSL = require('express-enforces-ssl');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 23201;
 const http = require('http');
 const WebSocket = require('ws').Server;
 
